fdc8c70468c63cd7d14fad287563ca9254a121ae
Victor Lopes <victor.cabrera0209@gmail.com>
2021-08-22 22:07:03 -0300

    1st Requirement second Fix

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,6 +1,6 @@
 db.movies.aggregate([{ $match: { $and: [
   { "imdb.rating": { $gte: 7 } },
-  { genres: { $ne: { $in: ["Crime", "Horror"] } } },
+  { genres: { $not: { $in: ["Crime", "Horror"] } } },
   { rated: { $all: ["PG", "G"] } },
   { languages: { $in: ["English", "Spanish"] } }],
 } }]);
