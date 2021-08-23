febfd37e2df5678652d622e2c06e2a4f0d198035
Victor Lopes <victor.cabrera0209@gmail.com>
2021-08-23 13:27:13 -0300

    7 Requirement

---
+++ challenges/desafio7.js
@@ -0,0 +1,35 @@
+db.movies.aggregate([
+  {
+    $match:
+    {
+      languages: "English",
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group:
+    {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $project:
+    {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  {
+    $sort:
+    {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+]);
+// Desenvolvido com ajuda de Marcelo Pelegrino e Lucas Martins
